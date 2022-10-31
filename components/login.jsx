2443526586d86c9258cbd446b97ab2506f8e26c1
rubiyet <rubiyetfardous@gmail.com>
2022-11-01 01:02:24 +0600

    tailwindcss added

---
+++ components/login.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Login() {
+    return (
+        <div className='bg-slate-600 flex justify-center h-screen w-screen text-base'>
+            <Image src="/login.png" alt='' width={650} height={450} />
+            a
+        </div>
+    )
+}
+    
