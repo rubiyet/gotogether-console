eb3fded7d49e9c202ae62ea4d27e09fbcc627087
rubiyet <rubiyetfardous@gmail.com>
2022-11-16 16:18:10 +0600

    home page slider height increased

--- components/HomePageSlider.jsx
+++ components/HomePageSlider.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function HomePageSlider() {
   return (
-    <div className="h-56 sm:h-64 xl:h-80">
+    <div className="h-56 sm:h-64 xl:h-[32rem]">
       <Carousel>
-      <Image src="/sliders/2.jpg" alt="" width={1500} height={0} />
-      <Image src="/sliders/3.jpg" alt="" width={1500} height={0} />
-      <Image src="/sliders/1.jpg" alt="" width={5500} height={0} />
+      <Image src="/sliders/8.jpg" alt="" width={1500} height={0} />
+      <Image src="/sliders/9.jpg" alt="" width={1500} height={0} />
+      <Image src="/sliders/7.jpg" alt="" width={5500} height={0} />
       </Carousel>
     </div>
   );
